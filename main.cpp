4d5c4658b7bce8800a04da769c6ed322215d7975
Delipt <arhimagforever@gmail.com>
2013-01-10 14:46:20 +0400

    new cat, now walk

--- main.cpp
+++ main.cpp
@@ -1,27 +1,63 @@
 #include <Irrlicht.h>
+#include "MyEventReceiver.h"
  using namespace irr;
 
  int main()
  {
-    IrrlichtDevice* device = createDevice(video::EDT_OPENGL,core::dimension2d<u32>(1280,720));
+    MyEventReceiver receiver;
+    IrrlichtDevice* device = createDevice(video::EDT_OPENGL,core::dimension2d<u32>(1280,720),32,false,false,true,&receiver);
     if(!device)
         return 1;
     video::IVideoDriver* driver  = device->getVideoDriver();
     scene::ISceneManager* svm = device->getSceneManager();
-    scene::IAnimatedMesh* cat = svm->getMesh("cat/cat.obj");
+    scene::IAnimatedMesh* cat = svm->getMesh("cat/cat.b3d");
     if(!cat)
     {
         device->drop();
         return 2;
     }
+    //add simple plane
+    scene::IMesh* plane = svm->addHillPlaneMesh("plane",core::dimension2d<f32>(200,200),core::dimension2d<u32>(20,20));
+    scene::IMeshSceneNode* planeNode = svm->addMeshSceneNode(plane);
+    planeNode->setMaterialFlag(video::EMF_LIGHTING,false);
+    planeNode->setMaterialTexture(0,driver->getTexture("texture/grass.jpg"));
     scene::IMeshSceneNode* node = svm->addMeshSceneNode(cat);
     if(node)
         node->setMaterialFlag(video::EMF_LIGHTING,false);
-    svm->addCameraSceneNode(0,core::vector3df(0,2,3),core::vector3df(0,0,0));
+    scene::ICameraSceneNode* camera = svm->addCameraSceneNode(node,core::vector3df(1,5,8.5),core::vector3df(0,0,0));
+    node->setRotation(core::vector3df(0,-90,0));
     while(device->run())
     {
         driver->beginScene(true,true,video::SColor(255,0,0,255));
         svm->drawAll();
+        if(receiver.IsKeyDown(KEY_KEY_W))
+        {
+             node->setPosition(node->getPosition()+core::vector3df(1,0,0));
+        }
+
+        if(receiver.IsKeyDown(KEY_KEY_S))
+        {
+            node->setPosition(node->getPosition()+core::vector3df(-1,0,0));
+        }
+
+        if(receiver.IsKeyDown(KEY_KEY_A))
+        {
+             node->setPosition(node->getPosition()+core::vector3df(0,0,1));
+        }
+
+        if(receiver.IsKeyDown(KEY_KEY_D))
+        {
+            node->setPosition(node->getPosition()+core::vector3df(0,0,-1));
+        }
+
+        //camera
+        camera->setTarget(node->getPosition());
+
+        if(receiver.IsKeyDown(KEY_ESCAPE))
+        {
+            exit(1);
+        }
+
         driver->endScene();
     }
     device->drop();
